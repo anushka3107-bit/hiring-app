73d5f56ae6d22831a0d9a4407b7d2719d29711bb
anushka3107-bit <bhardwajanushka180@gmail.com>
2023-10-31 01:57:02 +0530

    first commit

---
+++ src/containers/home/RequisitionDetailsForm.tsx
@@ -0,0 +1,123 @@
+import { Button, Flex, Box } from "@chakra-ui/react";
+import React, {useEffect} from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { useRouter } from "next/router"; // Import the useRouter hook
+import FormInput from "../../components/formComponents/FormInput";
+import FormSelect from "../../components/formComponents/FormSelect";
+import { IRequisitionDetails } from "../../interface/forms";
+import { genderOptions, urgencyOptions } from "./constants";
+import {useData} from "./DataProvider";
+
+
+type RequisitionDetailsProps = {
+  setTabIndex: (index: number) => void | number;
+};
+
+
+const RequisitionDetailsForm: React.FC<RequisitionDetailsProps> = ({
+  setTabIndex,
+}: RequisitionDetailsProps) => {
+  const router = useRouter(); // Initialize the useRouter hook
+  const dataContext = useData();
+
+  const {
+    handleChange,
+    errors,
+    touched,
+    handleBlur,
+    handleSubmit,
+    values,
+    setFieldTouched,
+    setFieldValue,
+  } = useFormik<IRequisitionDetails>({
+    initialValues: {
+      requisitionTitle: "",
+      noOfOpenings: 0,
+      urgency: "",
+      gender: "",
+    },
+    validationSchema: Yup.object().shape({
+      requisitionTitle: Yup.string().required("Requisition title is required"),
+      noOfOpenings: Yup.number()
+        .typeError("Enter a valid number")
+        .required("Number of openings is required")
+        .positive("Enter a valid number")
+        .min(1, "Enter a valid number"),
+      urgency: Yup.string().required("Urgency is required"),
+      gender: Yup.string().required("Gender is required"),
+    }),
+    onSubmit: (values) => {
+      // You can add any logic here before navigating
+      // For example, you can send the form data to the server
+      setTabIndex(1);
+    },
+  });
+
+  useEffect(() => {
+    if (dataContext) {
+      dataContext.setState((prevState: typeof dataContext.state) => ({
+        ...prevState,
+        requisitionDetails: values,
+      }));
+    }
+    console.log("formik values", values);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [values]);
+
+  return (
+    <Box width="100%" as="form" onSubmit={handleSubmit as any}>
+      <Box width="100%">
+        <FormInput
+          label="Requisition Title"
+          placeholder="Enter requisition title"
+          name="requisitionTitle"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values?.requisitionTitle}
+          error={errors?.requisitionTitle}
+          touched={touched?.requisitionTitle}
+        />
+        <FormInput
+          label="Number of openings"
+          placeholder="Enter number of openings"
+          name="noOfOpenings"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values?.noOfOpenings}
+          error={errors?.noOfOpenings}
+          touched={touched?.noOfOpenings}
+        />
+        <FormSelect
+          label="Gender"
+          name="gender"
+          placeholder="Select gender"
+          options={genderOptions}
+          onChange={setFieldValue}
+          onBlur={setFieldTouched}
+          error={errors.gender}
+          touched={touched.gender}
+          value={values.gender}
+        />
+        <FormSelect
+          label="Urgency"
+          name="urgency"
+          placeholder="Select urgency"
+          options={urgencyOptions}
+          onChange={setFieldValue}
+          onBlur={setFieldTouched}
+          error={errors.urgency}
+          touched={touched.urgency}
+          value={values.urgency}
+        />
+        <Flex w="100%" justify="flex-end" mt="4rem">
+          <Button colorScheme="red" type="submit">
+            Next
+          </Button>
+        </Flex>
+      </Box>
+    </Box>
+  );
+};
+
+export default RequisitionDetailsForm;
